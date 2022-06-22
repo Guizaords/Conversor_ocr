d0fd5ac0bf544265d3c1cf823e0990001fd48a88
Guizaords <rds.guilherme@gmail.com>
2022-06-22 07:29:25 -0300

    Incluído a funcionalidade de converter arquivos html para pdf

--- Main.py
+++ Main.py
@@ -1,13 +1,12 @@
 import tkinter.filedialog
 import shutil
 import ocrmypdf
+import pdfkit as pdfkit
 import pdfplumber
 import os
 import sys
-import re
 import PyPDF2
 from tkinter import *
-
 
 # PALETA DE CORES
 preto = "#000000"  # black
@@ -19,43 +18,15 @@ verde = "#59b356"  # green
 cinza = "#d9d9d9"  # grey
 
 impressao = []
-
-#os.chdir(sys._MEIPASS)
 data_path = os.path.join('tess_data','por.traineddata')
 home_path = os.getcwd()
 
-def cria_diretorio(pasta_ocr):
+def cria_diretorio(pasta):
     try:
-        ocr_path = os.mkdir(os.path.join(home_path, pasta_ocr))
+        ocr_path = os.mkdir(os.path.join(home_path, pasta))
         return ocr_path
     except FileExistsError:
         pass
-
-def create_ocr():
-    print('work in progress')
-    pdf_a_tratar = tkinter.filedialog.askopenfilename()
-
-    diretorio_ocr = cria_diretorio('OCR-CRIADOS')
-    pdf_alvo = shutil.copy2(pdf_a_tratar, home_path)
-    pdf_alvo_name = os.path.basename(pdf_alvo).replace(" ","")
-
-    i = ""
-    for s in pdf_alvo_name:
-        i=i+s
-    pdf_fim = str(i)
-
-    pdf_alvo = os.rename(pdf_alvo, pdf_alvo_name)
-
-    texto_tkinter.set("Seu arquivo será renomeado \npara "+pdf_alvo_name+ ".Aguarde" )
-
-    if __name__ == '__main__':
-        os.system('ocrmypdf -l eng+por --deskew --rotate-pages --skip-text' +' '+i + " "+ i)
-
-        shutil.move(pdf_alvo_name, os.path.join(home_path,'OCR-CRIADOS'))
-        texto_tkinter.set("Conversão concluída Enviando para a pasta OCR-CRIADOS")
-
-        #def texto_ocr(): #teste com ocrmypdf. para remover arquivo txt basta recomer a flag --sidecar output.txt
-        #os.system('ocrmypdf -l eng+por --deskew --rotate-pages --remove-background --sidecar vr.csv VR.pdf VR-OCR.pdf')
 
 def cria_lista_ocr():
 
@@ -68,7 +39,6 @@ def cria_lista_ocr():
     diretorio_ocr = cria_diretorio('OCR-CRIADOS')
 
     for pdf in tuplaDocs:
-        # print(os.path.basename(pdf))
         pdf_alvo = shutil.copy2(pdf, home_path)
         pdf_alvo_name = os.path.basename(pdf).replace(" ", "-")
 
@@ -76,120 +46,128 @@ def cria_lista_ocr():
         for s in pdf_alvo_name:
             i=i+s
         pdf_fim = str(i)
-
         pdf_alvo = os.rename(pdf_alvo, pdf_alvo_name)
-
         texto_tkinter.set("Seu arquivo será renomeado \npara "+pdf_alvo_name+ ".Aguarde" )
-
         if __name__ == '__main__':
             os.system('ocrmypdf -l eng+por --deskew --rotate-pages --skip-text' +' '+i + " "+ i)
-
-            #ocrmypdf.ocr('original','original.pdf', deskew=True, skip_text=True, rotate_pages=True)
-
             pdf_alvo = i
             shutil.move(pdf_alvo_name, os.path.join(home_path,'OCR-CRIADOS'))
             texto_tkinter.set(f'Conversão concluída\n Enviando {pdf_alvo_name}para a pasta OCR-CRIADOS')
 
 
-# def recorta_paginas_pesquisadas(): # base no Pypdf2
-#     path_to_pdf = extract_whole()
-#     with open(path_to_pdf, 'rb') as file:
-#         reader = PyPDF2.PdfFileReader(file)
-#         pdfWriter = PyPDF2.PdfFileWriter()
-#         pages = impressao
-#
-#         if len(impressao) > 0:
-#
-#             for page in pages:
-#                 pdfWriter.addPage(reader.getPage(page))
-#
-#             with open ('{0}_extraido.pdf'.format(path_to_pdf), 'wb') as f:
-#                 pdfWriter.write(f)
-#                 f.close()
-#
-#             texto_tkinter.set("Separação concluída")
-#
-#         else:
-#             texto_tkinter.set('não há páginas a serem impressas')
-#     impressao.clear()
-
-
 def merger(): # funciona bem
-    file_names = tkinter.filedialog.askopenfilenames()
+    file_names = tkinter.filedialog.askopenfilenames(title='selecione os arquivos PDF a serem unidos',
+                                                                  filetypes=[("pdf",".pdf")])
     mergerpdf = PyPDF2.PdfFileMerger()
-
+    diretorio_ocr = cria_diretorio('PDFS UNIDOS')
+    texto_tkinter.set("Juntando arquivos... aguarde")
     for pdf in file_names:
         with open(pdf, "rb") as file:
             mergerpdf.append(pdf)
-
     with open("merger.pdf", "wb") as f:
         mergerpdf.write(f)
+    shutil.move('merger.pdf', os.path.join(home_path, 'PDFS UNIDOS'))
+    texto_tkinter.set(f'Conversão concluída\n Enviado para a pasta PDFS UNIDOS')
 
-    texto_tkinter.set("Work in progress")
-
-def renameallSeiDocs():
+def converte_html_pdf():
     # Funcionou. Vai remover todos os pdfs da pasta e renomeá-los
-    seiDocs = tkinter.filedialog.askopenfilenames(title='selecione os pdfs a serem renomeados',
+    config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+    diretorio_html = cria_diretorio('HTML-CONVERTIDOS')
+    seiHtmls = tkinter.filedialog.askopenfilenames(title='selecione os arquivos HTML a serem convertidos',
+                                                                  filetypes=[("html",".html")])
+    texto_tkinter.set("Convertendo arquivos... aguarde")
+    for html in seiHtmls:
+        html_copy = shutil.copy2(html,home_path)
+        pdf = pdfkit.from_file(html_copy, f'{html_copy}.pdf', configuration=config)
+        os.remove(html_copy)
+
+    for pdf in os.listdir(home_path):
+        if 'pdf' in pdf:
+            shutil.move(pdf, os.path.join(home_path,'HTML-CONVERTIDOS'))
+
+    texto_tkinter.set(f'Conversão concluída\n Enviados para a pasta HTML-CONVERTIDOS')
+
+def extrai_e_recorta(): # com pdfplumber. Funciona melhor
+    nome_a_extrair = str(input_nome.get().lower())
+    if nome_a_extrair == "":
+        texto_tkinter.set("nome não pode ser vazio")
+        return
+    tupla_pdf = tkinter.filedialog.askopenfilenames(title='selecione os arquivos PDF a serem extraidos',
                                                                   filetypes=[("pdf",".pdf")])
+    diretorio_extraidos = cria_diretorio('PDF-EXTRAIDOS')
 
-    for pdf in seiDocs:
-        # print(os.path.basename(pdf))
-        cortado = pdf.split("-")
-        os.rename(pdf, cortado[1])
 
-def meta_data():
+    for _pdf in tupla_pdf:
+        documento_pdf = shutil.copy2(_pdf,home_path)
 
-    pdf = pdfplumber.open(str(tkinter.filedialog.askopenfilename()))
+        lista_pages = []
+        pdf = pdfplumber.open(documento_pdf)
+        reader = PyPDF2.PdfFileReader(documento_pdf)
+        pdfWriter = PyPDF2.PdfFileWriter()
+        n = len(pdf.pages)
+        nome_a_extrair = str(input_nome.get().lower())
+        print(nome_a_extrair)
 
-    print("Number of pages : {}".format(len(pdf.pages)))
-    print("Pages : {}".format(pdf.pages))
+        try:
+            for page in range(n):
+                data = pdf.pages[page].extract_text().lower()
+                if nome_a_extrair in data:
+                    lista_pages.append(page)
+                    print(lista_pages)
 
-    print("Document Information")
-    print(pdf.metadata)
+            texto_tkinter.set('nome encontrado em '+ str(lista_pages[-1])+ ' páginas.\n Extraindo...')
 
-    print("Author name : {}".format(pdf.metadata["Author"]))
-    print("Creator : {}".format(pdf.metadata["Creator"]))
+            if len(lista_pages) > 0:
+                for pagina in lista_pages:
+                    pdfWriter.addPage(reader.getPage(pagina))
 
-    pdf.close()
+                    with open('{0}_extraido.pdf'.format(documento_pdf), 'wb') as f:
+                        pdfWriter.write(f)
 
-# def extract_whole(): # com pdfplumber. Funciona melhor
-#     documento = tkinter.filedialog.askopenfilename()
-#     pdf = pdfplumber.open(documento)
-#     n = len(pdf.pages)
-#     nome_a_extrair = str(input_nome.get().lower())
-#     print(nome_a_extrair)
-#     regex = re.compile(nome_a_extrair, flags=re.I)
+                texto_tkinter.set("Separação concluída")
+
+            else:
+                texto_tkinter.set('não há páginas a serem impressas')
+
+        except:
+            pdf.close()
+            texto_tkinter.set('Erro. Nome não encontrado \n Tente novamente')
+            input_nome.delete(0,END)
+            return
+        lista_pages.clear()
+        pdf.close()
+    input_nome.delete(0, END)
+    for pdf in os.listdir(home_path):
+        if 'extraido.pdf' in pdf:
+            shutil.move(pdf, os.path.join(home_path,'PDF-EXTRAIDOS'))
+        elif '.pdf' in pdf:
+            os.remove(pdf)
+
+    texto_tkinter.set(f'Conversão concluída\n Enviados para a pasta PDF-EXTRAIDOS')
+
+# def renameallSeiDocs():
+#     # Funcionou. Vai remover todos os pdfs da pasta e renomeá-los
+#     seiDocs = tkinter.filedialog.askopenfilenames(title='selecione os pdfs a serem renomeados',
+#                                                                   filetypes=[("pdf",".pdf")])
 #
-#     try:
+#     for pdf in seiDocs:
+#         # print(os.path.basename(pdf))
+#         cortado = pdf.split("-")
+#         os.rename(pdf, cortado[1])
+# def meta_data():
 #
-#         for page in range(n):
+#     pdf = pdfplumber.open(str(tkinter.filedialog.askopenfilename()))
 #
-#         # for page in pdf.pages:
-#             data = pdf.pages[page].extract_text().lower()
-#             print(data)
-#             # if regex.search(data):
-#             #     impressao.append(page)
+#     print("Number of pages : {}".format(len(pdf.pages)))
+#     print("Pages : {}".format(pdf.pages))
 #
-#             if nome_a_extrair in data:
-#                 impressao.append(page)
+#     print("Document Information")
+#     print(pdf.metadata)
 #
-#         print(impressao)
-#         texto_tkinter.set('nome encontrado em '+ str(impressao[-1])+ ' páginas.\n Extraindo...')
-#         input_nome.delete(0,END)
-#
-#     except:
-#         pdf.close()
-#         texto_tkinter.set('Erro. Nome não encontrado \n Tente novamente')
-#         input_nome.delete(0,END)
-#
-#         return
+#     print("Author name : {}".format(pdf.metadata["Author"]))
+#     print("Creator : {}".format(pdf.metadata["Creator"]))
 #
 #     pdf.close()
-#     return  documento
-
-
-# usando tkinter (modularizar dp)
-
 janela = Tk()
 janela.geometry("400x400")
 janela.title("Tratamento de pdf")
@@ -207,28 +185,35 @@ label_selecionar = Label(frame1, text="O que você gostaria de fazer?",
                          width=24, height=1, anchor=CENTER)
 label_selecionar.grid(row=1,column=0,sticky=NSEW, pady=1)
 
-
-btn_renameSeiDocs = Button(frame1, text="RENOMAER DOCS SEI", command=renameallSeiDocs,
+#Botao que converter html para pdf
+btn_converter_html_pdf = Button(frame1, text="CONVERTER HTML EM PDF", command=converte_html_pdf,
                         width=60, height=1, anchor=CENTER, fg=branco, bg=verde)
-btn_renameSeiDocs.grid(row=3,column=0, sticky=NSEW, pady=1)
+btn_converter_html_pdf.grid(row=3,column=0, sticky=NSEW, pady=1)
 
-# btn_converter_pdf_para_ocr = Button(frame1, text="CONVERTER UM PDF EM FORMATO PESQUISAVEL", command=create_ocr,
-#                                     width=60, height=1, anchor=CENTER, bg=vermelho, fg=branco)
+# Botao que transforma pdf em pesquisavel
 
 btn_converter_pdf_para_ocr = Button(frame1, text="CONVERTER UM PDF EM FORMATO PESQUISAVEL", command=cria_lista_ocr,
                                     width=60, height=1, anchor=CENTER, bg=vermelho, fg=branco)
 btn_converter_pdf_para_ocr.grid(row=4,column=0, sticky=NSEW, pady=1)
 
-btn_extrair_pagina_nome = Button(frame1, text="JUNTAR PDF", command=merger,
+# Botao que faz o merge dos pdfs
+btn_juntar_pdf = Button(frame1, text="JUNTAR PDF", command=merger,
                                  width=60, height=1, anchor=CENTER, bg=azul, fg=branco)
-btn_extrair_pagina_nome.grid(row=5,column=0, sticky=NSEW, pady=1)
+btn_juntar_pdf.grid(row=5,column=0, sticky=NSEW, pady=1)
 
-# input_nome = Entry(frame1, width=60)
-# input_nome.grid(row=6,column=0, sticky=NSEW, pady=1)
 
-# label_pesquisar = Label(frame1, text="Para separar as páginas com base em um nome,\n digite acima o nome a ser pesquisado e depios\n CLIQUE no botão",
-#                          width=20, height=5, anchor=CENTER, fg=vermelho)
-# label_pesquisar.grid(row=7,column=0,sticky=NSEW, pady=1)
+# Botao que extrai as páginas
+btn_extrair_pagina_nome = Button(frame1, text="SEPARAR PDF COM BASE EM UM NOME", command=extrai_e_recorta,
+                                 width=60, height=1, anchor=CENTER, bg=preto, fg=branco)
+btn_extrair_pagina_nome.grid(row=7,column=0, sticky=NSEW, pady=1)
+
+
+input_nome = Entry(frame1, width=60)
+input_nome.grid(row=6,column=0, sticky=NSEW, pady=1)
+
+label_pesquisar = Label(frame1, text="Para separar as páginas com base em um nome,\n digite acima o nome a ser pesquisado e depios\n CLIQUE no botão",
+                         width=20, height=5, anchor=CENTER, fg=vermelho)
+label_pesquisar.grid(row=9,column=0,sticky=NSEW, pady=1)
 
 
 #Informações de LOG
